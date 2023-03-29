01afb97569933935a5c38be5b07a1c29bb853f15
canknbr <cankanbur1@gmail.com>
2023-03-29 17:35:57 +0300

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Can Kanbur on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
